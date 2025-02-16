6e216c5e3c9cc7111df9914125ab5ced74c42cbe
azure1307 <azure1307@gmail.com>
2025-02-16 22:28:59 +0900

    0216 변수 1일차 시작

---
+++ src/variable/Var3.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var3 {
+    public static void main(String[] args) {
+        int a; // 변수 선언
+        a = 10; // 변수 초기화. a에 10 저장
+        System.out.println(a); // 10
+        a = 50; // 변수 값 변경
+        System.out.println(a); // 50
+    }
+}
