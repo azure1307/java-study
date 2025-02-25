ddc891510762e6bcf7fcc9acce3684eb6f6f7984
azure1307 <azure1307@gmail.com>
2025-02-26 00:27:54 +0900

    0226 반복문 2일차 끝

---
+++ src/loop/ex/ForEx2.java
@@ -0,0 +1,9 @@
+package loop.ex;
+
+public class ForEx2 {
+    public static void main(String[] args) {
+        for (int i = 2; i < 21; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
